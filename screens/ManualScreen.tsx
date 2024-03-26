cd7bbc874ace41a7c4d9b0fa73189f8db31470bb
Samuel Guerrero <107590310+SamuelGuerrero@users.noreply.github.com>
2024-03-25 23:30:39 -0600

    📝 Added confiability bar in manual screen

--- screens/ManualScreen.tsx
+++ screens/ManualScreen.tsx
@@ -25,7 +25,7 @@ export default function ManualScreen() {
 
   const step1 = {
     title: "Preparar la captura",
-    instuctions: [
+    instructions: [
       "Selecciona la opción: Tomar foto 📷",
       "Encuentra un entorno bien iluminado para garantizar una captura clara ☀️",
       "Coloca la Tilapia de manera que las papilas genitales estén visibles 👁️",
@@ -35,7 +35,7 @@ export default function ManualScreen() {
 
   const step2 = {
     title: "Enfoca la cámara",
-    instuctions: [
+    instructions: [
       "Asegúrate de que la cámara esté enfocada en la papila genital y que ésta se encuentre en medio del recuadro negro ✅",
       "Puedes configurar el zoom de la cámara con el deslizador ubicado debajo del recuadro negro 🔎",
       "Toma la fotografía 📷",
@@ -44,7 +44,7 @@ export default function ManualScreen() {
 
   const step3 = {
     title: "Predice el valor",
-    instuctions: [
+    instructions: [
       <ListText>
         Una vez capturada la imágen, presiona el botón{" "}
         <Text style={{ fontWeight: "700", color: "#2E86C1" }}>Predecir</Text>{" "}
@@ -61,14 +61,15 @@ export default function ManualScreen() {
         <Text style={{ fontWeight: "700", color: "#CD5C5C" }}>Rosa</Text> para
         Hembra y color{" "}
         <Text style={{ fontWeight: "700", color: "#2E86C1" }}>Azul</Text> para
-        Macho como se ve en la siguiente imagen 🏷️
+        Macho, al igual que la barra de confiabilidad como se ve en la siguiente
+        imagen 🏷️
       </ListText>,
     ],
   };
 
   const stepFile1 = {
     title: "Carga las imágenes",
-    instuctions: [
+    instructions: [
       <ListText>Selecciona la opción: Cargar archivos 📁</ListText>,
 
       <ListText>
@@ -87,14 +88,14 @@ export default function ManualScreen() {
 
   const stepFile2 = {
     title: "Termina de cargar las imágenes",
-    instuctions: [
+    instructions: [
       "Una vez cargadas y recortadas las imágenes, presiona Cancelar para ver los resultados en la parte inferior de la imagen cargada como se ve en la siguiente imagen ✅",
     ],
   };
 
   const stepFile3 = {
     title: "Vuelve a cargar nuevas imágenes 🔙",
-    instuctions: [
+    instructions: [
       <ListText>
         Una vez terminada la predicción de las imágenes cargadas, presiona el
         botón{" "}
@@ -132,7 +133,7 @@ export default function ManualScreen() {
           <View style={styles.cardOption}>
             <ManualItemTitle title={step1.title} itemNumber={1} />
             <View style={styles.listSection}>
-              {step1.instuctions.map((rule, index) => (
+              {step1.instructions.map((rule, index) => (
                 <ListText key={index}>{rule}</ListText>
               ))}
             </View>
@@ -142,7 +143,7 @@ export default function ManualScreen() {
           <View style={styles.cardOption}>
             <ManualItemTitle title={step2.title} itemNumber={2} />
             <View style={styles.listSection}>
-              {step2.instuctions.map((rule, index) => (
+              {step2.instructions.map((rule, index) => (
                 <ListText key={index}>{rule}</ListText>
               ))}
             </View>
@@ -167,7 +168,7 @@ export default function ManualScreen() {
           <View style={styles.cardOption}>
             <ManualItemTitle title={step3.title} itemNumber={3} />
             <View style={styles.listSection}>
-              {step3.instuctions.map((rule, index) =>
+              {step3.instructions.map((rule, index) =>
                 cloneElement(rule, { key: index }),
               )}
             </View>
@@ -176,6 +177,73 @@ export default function ManualScreen() {
                 resizeMode="contain"
                 style={[styles.example, { width: calculatedWidth }]}
                 source={require("../assets/Example2.jpeg")}
+              />
+            </View>
+            <View style={styles.listSection}>
+              <ListText>
+                Esta barra de confiabilidad indica lo siguiente:
+              </ListText>
+              <View style={{ marginBottom: 15 }}>
+                <Text
+                  style={{
+                    color: "#FFF",
+                    marginLeft: 25,
+                    fontSize: 18,
+                    textAlign: "justify",
+                  }}
+                >
+                  🔴 Color{" "}
+                  <Text style={{ color: "#FE2020", fontWeight: "600" }}>
+                    Rojo
+                  </Text>
+                  : un valor de predicción entre 0% y 60%. Este valor indica un
+                  valor de predicción demasiado bajo, se sugiere tomar otra
+                  captura.
+                </Text>
+              </View>
+              <View style={{ marginBottom: 15 }}>
+                <Text
+                  style={{
+                    color: "#FFF",
+                    marginLeft: 25,
+                    fontSize: 18,
+                    textAlign: "justify",
+                  }}
+                >
+                  🟡 Color{" "}
+                  <Text style={{ color: "#FFFF00", fontWeight: "600" }}>
+                    Amarillo
+                  </Text>
+                  : un valor de predicción entre 61% y 80%. Este valor indica
+                  una predicción moderada, se podría considerar tomar otra
+                  captura para mayor precisión.
+                </Text>
+              </View>
+              <View>
+                <Text
+                  style={{
+                    color: "#FFF",
+                    marginLeft: 25,
+                    fontSize: 18,
+                    textAlign: "justify",
+                  }}
+                >
+                  🟢 Color{" "}
+                  <Text style={{ color: "#008000", fontWeight: "600" }}>
+                    Verde
+                  </Text>
+                  : un valor de predicción entre 81% y 100%. Este valor indica
+                  una alta confiabilidad en la predicción, no es necesario tomar
+                  otra captura.
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.exampleContainer}>
+              <Image
+                resizeMode="contain"
+                style={{ width: 320 }}
+                source={require("../assets/BarraConfiabilidadRN.jpg")}
               />
             </View>
           </View>
@@ -190,7 +258,7 @@ export default function ManualScreen() {
           <View style={styles.cardOption}>
             <ManualItemTitle title={stepFile1.title} itemNumber={1} />
             <View style={styles.listSection}>
-              {stepFile1.instuctions.map((rule, index) =>
+              {stepFile1.instructions.map((rule, index) =>
                 cloneElement(rule, { key: index }),
               )}
             </View>
@@ -201,7 +269,7 @@ export default function ManualScreen() {
           <View style={styles.cardOption}>
             <ManualItemTitle title={stepFile2.title} itemNumber={2} />
             <View style={styles.listSection}>
-              {stepFile2.instuctions.map((rule, index) => (
+              {stepFile2.instructions.map((rule, index) => (
                 <ListText key={index}>{rule}</ListText>
               ))}
             </View>
@@ -212,7 +280,7 @@ export default function ManualScreen() {
           <View style={styles.cardOption}>
             <ManualItemTitle title={stepFile3.title} itemNumber={3} />
             <View style={styles.listSection}>
-              {stepFile3.instuctions.map((rule, index) =>
+              {stepFile3.instructions.map((rule, index) =>
                 cloneElement(rule, { key: index }),
               )}
             </View>
